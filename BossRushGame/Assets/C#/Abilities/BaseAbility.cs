f4afe1e835b907ebd7df2a4215197773847bff44
Max Louhio <mlouhio@hotmail.com>
2018-10-11 11:58:20 +0300

    Base Ability has now methods for dealing damage and ending turn

--- BossRushGame/Assets/C#/Abilities/BaseAbility.cs
+++ BossRushGame/Assets/C#/Abilities/BaseAbility.cs
@@ -7,15 +7,40 @@ public class BaseAbility : MonoBehaviour
     [SerializeField]
     public UnitHighlight.Targets InitTarget;
 
-    public BattleUnitBase Attacker;
-    public BattleUnitBase Defender;
+    public BattleUnitBase Attacker; //Who attacks
+    public BattleUnitBase Defender; //Who gets attacked
     public int damage;
 
     public void Act(GameObject go)
     {
-        go.GetComponent<CharCombatValues>().TakeDamage(damage);
+        Attacker = FindObjectOfType<BattleSystem_v2>().GetUnitTurn();
+        Defender = go.GetComponent<BattleUnitBase>();
 
-        //PURKKAAAAA
+        StartCoroutine("MoveToTarget");
+    }
+
+    public IEnumerator MoveToTarget()
+    {
+        float timer = 0;
+        Vector3 startPos = Attacker.transform.position;
+        Vector3 endPos = Defender.transform.position;
+        while (timer < 1)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, timer);
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+        DealDamage();
+        EndTurn();
+    }
+
+    public void DealDamage()
+    {
+        Defender.GetComponent<CharCombatValues>().TakeDamage(damage);
+    }
+
+    private void EndTurn()
+    {
         FindObjectOfType<BattleSystem_v2>().GetUnitTurn().EndTurn();
     }
 }
